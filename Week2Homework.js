767c1cc2309ef0e34259f07429a10485db6acc65
cplazajr <cesarplazajr@gmail.com>
2023-03-31 11:55:52 -0400

    Week2Homework

---
+++ Week2Homework.js
@@ -0,0 +1,17 @@
+let pizzaPlace = "Majin Bu";
+
+let numberOfToppings = 3;
+
+console.log(pizzaPlace, typeof pizzaPlace);
+
+console.log(numberOfToppings, typeof numberOfToppings);
+
+console.log(
+  `Welcome to ${pizzaPlace}, Here we serve ${numberOfToppings} types of toppings for your pizza.`
+);
+
+if (3 < 10) {
+  console.log("Quality, not quantity.");
+} else if (3 > 10) {
+  console.log("A whole lot of pizza.");
+}
